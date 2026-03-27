91f30236559704ed670b440d55525b7e1f1b5a28
indalbind <indalbind4562@gmail.com>
2026-03-27 19:29:15 +0530

    error handling in the Signup page

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+ 
 const CaptainSignup = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
